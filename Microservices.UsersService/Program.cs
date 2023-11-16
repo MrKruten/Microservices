954e5b230a8451c0637ffcb292c6bb011d0823f1
Khazov Daniil <80253384+MrKruten@users.noreply.github.com>
2023-11-16 13:23:02 +0700

    feat: db commands

--- Microservices.UsersService/Program.cs
+++ Microservices.UsersService/Program.cs
@@ -9,7 +9,9 @@ namespace Microservices.UsersService
 {
     // lab db
     // docker exec -t db pg_dumpall -c -U admin > dump.sql
-    // docker exec db psql -h localhost -d postgres -U admin -c 'CREATE DATABASE laba'
+    // docker exec db psql -h localhost -d laba -U admin -W admin -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE pid <> pg_backend_pid() AND datname = 'laba'"
+    // docker exec db psql -h localhost -d postgres -U admin -c "DROP DATABASE laba"
+    // docker exec db psql -h localhost -d postgres -U admin -c "CREATE DATABASE laba"
     // cat dump.sql | docker exec -i db psql -h localhost -U admin -d laba
 
     public class Program
